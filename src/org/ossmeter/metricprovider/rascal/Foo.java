9592798a06679c240f112b4282d1c22afa592936
James Williams <jamesrobertwilliams@gmail.com>
2013-04-11 14:49:42 +0200

    Generated Pongos.

--- src/org/ossmeter/metricprovider/rascal/Foo.java
+++ src/org/ossmeter/metricprovider/rascal/Foo.java
@@ -1,16 +1,13 @@
 package org.ossmeter.metricprovider.rascal;
 
-import java.io.File;
-
-import com.googlecode.pongo.PongoGenerator;
 
 public class Foo {
   public static void main(String[] args) {
-    try {
-      new PongoGenerator().generate(new File("/Users/jurgenv/EclipseWorkspace/org.ossmeter.metricprovider.rascal/src/rascalmetric.emf"));
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+	  RascalMetrics rm = new RascalMetrics();
+	  IntegerMeasurement test = new IntegerMeasurement();
+	  test.setMetric("org::rascal::foo/bar");
+	  test.setUri("class::/Foo.java");
+	  test.setValue(33);
+	  rm.getMeasurements().add(test);
   }
 }
